e0fb0616d84c90e4bb910dc0b33decd544111fa5
boucher <rboucher@gmail.com>
2014-03-30 10:37:07 -0700

    Serve the index file.

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import abort, redirect, url_for, session, request, render_template, R
 
 #from rauth.service import OAuth1Service, OAuth1Session
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="src/static", template_folder="src/html")
 
 
 @app.route('/upload')
@@ -14,3 +14,7 @@ def generate_large_csv():
     #    for row in iter_all_rows():
     #        yield ','.join(row) + '\n'
     #return Response(generate(), mimetype='text/csv')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
