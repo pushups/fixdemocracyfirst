937b37c7d1551554db8af97fdcf16f870c32c3cd
Chris Bucchere <chris@bucchere.com>
2015-04-11 13:32:29 -0700

    patches tests to account for datepicker's format

--- test/controllers/statements_controller_test.rb
+++ test/controllers/statements_controller_test.rb
@@ -18,7 +18,7 @@ class StatementsControllerTest < ActionController::TestCase
 
   test "should create statement" do
     assert_difference('Statement.count') do
-      post :create, statement: { approved: @statement.approved, campaign_id: @statement.campaign_id, candidate_id: @statement.candidate_id, description: @statement.description, event_day_id: @statement.event_day_id, rwu_id: @statement.rwu_id, title: @statement.title, ugc_candidate_name: @statement.ugc_candidate_name, ugc_date: @statement.ugc_date, ugc_event_location: @statement.ugc_event_location, ugc_event_title: @statement.ugc_event_title, ugc_notes: @statement.ugc_notes, url: @statement.url, user_id: @statement.user_id }
+      post :create, statement: { approved: @statement.approved, campaign_id: @statement.campaign_id, candidate_id: @statement.candidate_id, description: @statement.description, event_day_id: @statement.event_day_id, rwu_id: @statement.rwu_id, title: @statement.title, ugc_candidate_name: @statement.ugc_candidate_name, ugc_date: @statement.ugc_date.strftime('%m/%d/%Y'), ugc_event_location: @statement.ugc_event_location, ugc_event_title: @statement.ugc_event_title, ugc_notes: @statement.ugc_notes, url: @statement.url, user_id: @statement.user_id }
     end
 
     assert_redirected_to statement_path(assigns(:statement))
@@ -35,7 +35,7 @@ class StatementsControllerTest < ActionController::TestCase
   end
 
   test "should update statement" do
-    patch :update, id: @statement, statement: { approved: @statement.approved, campaign_id: @statement.campaign_id, candidate_id: @statement.candidate_id, description: @statement.description, event_day_id: @statement.event_day_id, rwu_id: @statement.rwu_id, title: @statement.title, ugc_candidate_name: @statement.ugc_candidate_name, ugc_date: @statement.ugc_date, ugc_event_location: @statement.ugc_event_location, ugc_event_title: @statement.ugc_event_title, ugc_notes: @statement.ugc_notes, url: @statement.url, user_id: @statement.user_id }
+    patch :update, id: @statement, statement: { approved: @statement.approved, campaign_id: @statement.campaign_id, candidate_id: @statement.candidate_id, description: @statement.description, event_day_id: @statement.event_day_id, rwu_id: @statement.rwu_id, title: @statement.title, ugc_candidate_name: @statement.ugc_candidate_name, ugc_date: @statement.ugc_date.strftime('%m/%d/%Y'), ugc_event_location: @statement.ugc_event_location, ugc_event_title: @statement.ugc_event_title, ugc_notes: @statement.ugc_notes, url: @statement.url, user_id: @statement.user_id }
     assert_redirected_to statement_path(assigns(:statement))
   end
 
