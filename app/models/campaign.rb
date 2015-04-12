24c76d6a77e50df2b18b1ad82fc20f71d7d302cb
Chris Bucchere <chris@bucchere.com>
2015-04-11 17:35:44 -0700

    adds convenience methods for nested models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -3,4 +3,17 @@ class Campaign < ActiveRecord::Base
   has_many :statements
   belongs_to :election
   belongs_to :candidate
+  
+  attr_reader :candidate_name
+  attr_reader :election_name
+
+  def candidate_name
+    c = self.candidate
+    c ? c.person_name : '-- Candidate Not Set --'
+  end
+
+  def election_name
+    e = self.election
+    e ? e.name : '-- Election Not Set --'
+  end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -3,4 +3,10 @@ class Candidate < ActiveRecord::Base
   belongs_to :person
   has_many :statements
   has_one :campaign
+  attr_reader :person_name
+
+  def person_name
+    p = self.person
+    p ? "#{p.first_name} #{p.last_name}" : "-- Name Not Set --"
+  end
 end
