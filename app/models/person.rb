b8ad765702a09ad99136b40f99192ce5b3d8fbd2
Chris Bucchere <chris@bucchere.com>
2015-04-12 10:33:24 -0700

    adds full_name

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,11 @@
 class Person < ActiveRecord::Base
   include DirtyColumns
+
   has_many :candidates #candidacies 
+
+  attr_reader :full_name
+
+  def full_name(include_middle = false)
+    "#{self.first_name}#{include_middle ? ' ' + self.middle_name : ''} #{self.last_name}"
+  end
 end
