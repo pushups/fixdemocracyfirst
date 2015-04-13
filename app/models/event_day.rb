1e546fb0fe576c650cae22df4061c1e2d4afc3fd
Chris Bucchere <chris@bucchere.com>
2015-04-13 15:11:49 -0700

    adds validation that end_time > start_time

--- app/models/event_day.rb
+++ app/models/event_day.rb
@@ -1,7 +1,10 @@
 class EventDay < ActiveRecord::Base
   include DirtyColumns
+
   belongs_to :event
   has_many :statements
+
+  validate :end_time_must_come_after_start_time
 
   before_save { |e| e.event_id = nil if e.event_id < 1 }
 
@@ -10,5 +13,13 @@ class EventDay < ActiveRecord::Base
   def event_name
     e = self.event
     e ? "#{e.title}" : "-- No Event --"
+  end
+   
+  private
+  
+  def end_time_must_come_after_start_time
+    if end_time.present? and end_time.present? and start_time > end_time
+      errors.add(:end_time, "must come after start time")
+    end
   end
 end
