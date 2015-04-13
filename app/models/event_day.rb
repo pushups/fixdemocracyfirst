981e139bfca03c4e3558a6e70003d5e861b98ed9
Chris Bucchere <chris@bucchere.com>
2015-04-12 19:43:07 -0700

    first pass at admin ui cleanup

--- app/models/event_day.rb
+++ app/models/event_day.rb
@@ -2,4 +2,13 @@ class EventDay < ActiveRecord::Base
   include DirtyColumns
   belongs_to :event
   has_many :statements
+
+  before_save { |e| e.event_id = nil if e.event_id < 1 }
+
+  attr_reader :event_name
+
+  def event_name
+    e = self.event
+    e ? "#{e.title}" : "-- No Event --"
+  end
 end
