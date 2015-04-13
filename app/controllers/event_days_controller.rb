3d5bf03ecf2492fb30f1742e9194f6699a791c04
Chris Bucchere <chris@bucchere.com>
2015-04-13 16:59:49 -0700

    cleans and DRYs up date massaging for event_day

--- app/controllers/event_days_controller.rb
+++ app/controllers/event_days_controller.rb
@@ -16,7 +16,6 @@ class EventDaysController < ApplicationController
   # GET /event_days/new
   def new
     @event_day = EventDay.new
-    @event_day.date = DateTime.now
   end
 
   # GET /event_days/1/edit
@@ -26,8 +25,7 @@ class EventDaysController < ApplicationController
   # POST /event_days
   # POST /event_days.json
   def create
-    @event_day = EventDay.new(event_day_params)
-    @event_day.date = !event_day_params[:date].empty? ? DateTime.strptime(event_day_params[:date], '%m/%d/%Y').to_date : nil
+    @event_day = EventDay.new(format_dates_and_times(event_day_params))
 
     respond_to do |format|
       if @event_day.save
@@ -43,24 +41,8 @@ class EventDaysController < ApplicationController
   # PATCH/PUT /event_days/1
   # PATCH/PUT /event_days/1.json
   def update
-    respond_to do |format|
-      #this is a little kludgy, but i couldn't figure out how to hack event_day_params w/o cloning it
-      p = event_day_params.clone
-      if !event_day_params[:date].empty?
-        #parse and reformat the date to remove timezone and put in a good format for the db
-        p[:date] = DateTime.strptime(event_day_params[:date], '%m/%d/%Y').to_date.strftime('%m/%d/%Y')
-      else
-        p[:date] = DateTime.now.to_date.strftime('%m/%d/%Y')
-      end        
-
-      #similarly, parse and reformat the start and end times, combining them with the event day's date
-      [:start_time, :end_time].each do |t| 
-        if !event_day_params[t].empty?
-          p[t] = DateTime.strptime("#{p[:date]} #{event_day_params[t]} #{AMERICA_NEW_YORK_TIME_ZONE.abbr}", "%m/%d/%Y %H:%M %p %Z")
-        end
-      end
-      
-      if @event_day.update(p)
+    respond_to do |format|      
+      if @event_day.update(format_dates_and_times(event_day_params))
         format.html { redirect_to @event_day, notice: 'Event day was successfully updated.' }
         format.json { render :show, status: :ok, location: @event_day }
       else
@@ -89,5 +71,23 @@ class EventDaysController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_day_params
       params.require(:event_day).permit(:rwu_id, :event_id, :date, :start_time, :end_time)
+    end
+    
+    def format_dates_and_times(params)
+      p = params.clone
+
+      if !params[:date].empty?
+        #parse and reformat the date to remove timezone and put in a good format for the db
+        p[:date] = DateTime.strptime(params[:date], '%m/%d/%Y').to_date.strftime
+      end
+            
+      #similarly, parse and reformat the start and end times, combining them with the event day's date
+      [:start_time, :end_time].each do |t|
+        if !params[t].empty?
+          p[t] = DateTime.strptime("#{!p[:date].empty? ? p[:date] : Date.today.strftime} #{params[t]} #{AMERICA_NEW_YORK_TIME_ZONE.abbr}",
+                                   "%Y-%m-%d %H:%M %p %Z")
+        end
+      end
+      p
     end
 end
