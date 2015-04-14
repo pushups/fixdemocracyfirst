a9df94336f2441eb6774dda79bbff90e1b94f123
Chris Bucchere <chris@bucchere.com>
2015-04-14 00:21:12 -0700

    cleans up admin ui for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,15 @@
 class Event < ActiveRecord::Base
   include DirtyColumns
+  
   belongs_to :venue
   has_many :event_days
   has_many :attendees
   has_many :users, through: :attendees
+  
+  attr_reader :venue_name
+  
+  def venue_name
+    v = self.venue
+    v ? v.name : '-- No Venue --'
+  end
 end
