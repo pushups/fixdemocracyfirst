b5b12a23fa35dd8e662b3e0d81cb18b67d2f9282
Chris Bucchere <chris@bucchere.com>
2015-04-15 01:01:04 -0700

    works around namespace collision with users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_user
+  before_filter :set_current_user
   
-  def set_user
+  def set_current_user
     #try to get the user from the rails session
-    return @user if @user
+    return @current_user if @current_user
     
     #if that fails, try to get the user_id from a cookie
-    user_id = cookies[:user_id]
+    user_id = cookies[:current_user_id]
     
     #try to find the user
     if user_id
       begin
-        @user = User.find(user_id)
-        return @user
+        @current_user = User.find(user_id)
+        return @current_user
       rescue Exception => e
         #user not found shouldn't mask other possible errors
         raise e unless e.is_a? ActiveRecord::RecordNotFound
@@ -24,12 +24,12 @@ class ApplicationController < ActionController::Base
     
     #if the user_id wasn't cookied or the cookied user_id wasn't found, 
     #we'll end up here, so create a new "guest" user and cookie its id
-    @user = User.create
-    cookies[:user_id] = { value: @user.id, :expires => Time.now + 365 * 24 * 60 * 60 }
-    @user
+    @current_user = User.create
+    cookies[:current_user_id] = { value: @current_user.id, :expires => Time.now + 365 * 24 * 60 * 60 }
+    @current_user
   end
   
   def require_admin
-    redirect_to '/' unless @user.admin?
+    redirect_to '/' unless @current_user.admin?
   end
 end
