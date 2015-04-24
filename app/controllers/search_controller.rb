ff5a77bec84b106ceeb14d6cfb788ff51c272394
Chris Bucchere <chris@bucchere.com>
2015-04-23 21:28:09 -0700

    fixes a copy pasta error

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def search
     flash[:alert] = "Please enter a candidate or event name" if params[:q].blank?
     @events = Event.search(params[:q]).records.to_a
-    @candidates = Event.search(params[:q]).records.to_a    
+    @candidates = Candidate.search(params[:q]).records.to_a    
   end
 end
 
