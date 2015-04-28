9ca9e4d1dae8715c1760ecdeaa2b1448848b726d
Chris Bucchere <chris@bucchere.com>
2015-04-28 12:51:12 -0700

    exports s3 bucket to a variable

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,3 @@
 #TODO export these to ENV variables (which don't seem to be available from initializers, d'oh)
 #S3_BUCKET = Aws::S3::Resource.new(region: ENV['AWS_REGION']).bucket(ENV['S3_BUCKET'])
-S3_BUCKET = Aws::S3::Resource.new(region: 'us-east-1').bucket('questionr-stage')
+S3_BUCKET = Aws::S3::Resource.new(region: 'us-east-1').bucket(ENV['S3_BUCKET'])
